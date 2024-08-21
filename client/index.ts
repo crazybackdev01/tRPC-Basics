6a46f10c2046c7d9a0a5b9f251bba1bedc2b418a
Udit Nagar <uditnagar419@gmail.com>
2024-08-21 23:46:53 +0530

    update gitignore

--- client/index.ts
+++ client/index.ts
@@ -3,13 +3,16 @@ import type { AppRouter } from "../server";
 //     👆 **type-only** import
 // Pass AppRouter as generic here. 👇 This lets the `trpc` object know
 // what procedures are available on the server and their input/output types.
-const trpc = createTRPCClient<AppRouter>({
+export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
       url: "http://localhost:3000",
     }),
   ],
 });
+
+// Just make a client.ts file in the React root folder or in some folder like "config" then export "trpc"
+// Export this above trpc variable wherever in React code file when need to call the server
 
 const main = async () => {
   const response = await trpc.createTodo.mutate({
